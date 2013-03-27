9757f9c909be635d098f7ab47de94aef0a425487
mfreytag <mfreytag@gmail.com>
2013-03-27 15:35:17 +0100

    added template

---
+++ template.java
@@ -0,0 +1,22 @@
+/*
+  ###PROGRAMMKOPF###
+  
+  Title
+  
+  FS23
+  Marc Freytag
+  Version: 1
+  20130101
+  Description
+
+*/
+
+public class template
+{
+  // ###HAUPTFUNKTION###
+  public static void main(String[] args )
+  {
+    // ###DEKLARATIONSTEIL###
+
+  }
+}
